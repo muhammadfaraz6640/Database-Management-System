662291628272a01dee930389a4449608605a57f0
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-07-09 19:57:46 +0500

    Between operator and some logical operators

---
+++ Between_and_logical Operators.sql
@@ -0,0 +1,3 @@
+select * from staff where CurrentSalary between 15000 AND 25000
+select * from staff where CurrentSalary >= 15000 AND CurrentSalary <= 25000
+select * from staff where StaffShift = 'morning' OR Gender = 'female'
