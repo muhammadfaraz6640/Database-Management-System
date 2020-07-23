0d1401db17e32758a1968be65a967f8d0f1192c4
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-07-23 11:21:16 +0500

    Domain Constraints with check statement

---
+++ Domain Constraints with check statement.sql
@@ -0,0 +1,10 @@
+create table Domain_Constraints(
+	sex char not null
+	check(sex IN ('M','F'))
+)
+
+insert into Domain_Constraints values('g') /*The INSERT statement conflicted with the CHECK constraint "CK__Domain_Cons__sex__6383C8BA" hence terminated*/
+
+insert into Domain_Constraints values('M')
+
+select * from Domain_Constraints
