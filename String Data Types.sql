c3c0c5003951d53337bfc1f30216ad178f17810a
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-07-23 11:39:13 +0500

    correcting mistake

--- String Data Types.sql
+++ String Data Types.sql
@@ -1,6 +1,6 @@
 create table StringDataTypes(
 	characters char(10), /* it takes fixed length storage of 10 bits in ram either you enter only 5 characters memory consumes more and extra  */
-	char_varchar varchar(100), /* it takes variable length storage of 100 characters with maximium of 800 characters it takes memory that you enters */
+	char_varchar nvarchar(100), /* it takes variable length storage of 100 characters with maximium of 800 characters it takes memory that you enters */
 	char_text text, /*stores data upto 2GB*/
-	char_max varchar(max), /*no limit with variable length stroage*/
+	char_max nvarchar(max), /*no limit with variable length stroage*/
 );
